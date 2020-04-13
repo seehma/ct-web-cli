5bc4c570c39ddbf7d31ac263cfcc5ab4a0cafe44
seehma <seehma@gmail.com>
2020-04-13 16:45:13 +0200

    first changes as published in ct-tut

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
+
 import (
 	"fmt"
+
+	"github.com/mmcdole/gofeed"
 )
+
 func main() {
-	fmt.Println("Hello World!")
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL("https://www.heise.de/rss/heise-atom.xml")
+	for i := 0; i < 5; i++ {
+		fmt.Println(feed.Items[i].Title)
+	}
 }
